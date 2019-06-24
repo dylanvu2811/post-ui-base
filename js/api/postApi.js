37ed18bf4dbb13feba8adc2ba55d2e650670b0d0
Duong <duongvu281194@gmail.com>
2019-06-24 10:26:57 +0700

    render posts lists

--- js/api/postApi.js
+++ js/api/postApi.js
@@ -1,34 +1,12 @@
-import fetchClient from "./fetchClient.js";
-import AppConstants from "../appConstants.js";
+import base from "./base.js";
 
-class PostApi {
+class PostApi extends base {
     getResourceName() {
         return 'posts';
     }
-    getAll() {
-        const url = `${AppConstants.API_URL}/${this.getResourceName()}`;
-        fetchClient.get(url);
-    }
 
-    getDetail(id) {
-        const url = `${AppConstants.API_URL}/${this.getResourceName()}/${id}`;
-        fetchClient.get(url);
-    }
 
-    add(post) {
-        const url = `${AppConstants.API_URL}/${this.getResourceName()}`;
-        fetchClient.post(url, post);
-    }
 
-    update(post) {
-        const url = `${AppConstants.API_URL}/${this.getResourceName()}/${post.id}`;
-        fetchClient.patch(url, post); 
-    }
-
-    remove(id) {
-        const url = `${AppConstants.API_URL}/${this.getResourceName()}/${id}`;
-        fetchClient.delete(url);
-    }
 }
 const postApi = new PostApi();
 export default postApi;
