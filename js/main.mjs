52b37b5180388a054b56004981ca1c3586900335
Duong <duongvu281194@gmail.com>
2019-06-24 12:24:43 +0700

    set info item

--- js/main.mjs
+++ js/main.mjs
@@ -1,11 +1,43 @@
 'use strict';
 import postApi from './api/postApi.js';
+import utils from "./utils.js";
 
 // create a element
 const buildPostItem = (post) => {
   const postItemTemplate = document.querySelector('#itemPost');
   const postItemFragment = postItemTemplate.content.cloneNode(true);
   const postItem = postItemFragment.querySelector('li');
+
+  // set images
+  const imagesItem = postItemFragment.querySelector('.card img');
+  if (imagesItem) {
+    imagesItem.src = post.imageUrl;
+  }
+
+  // set title
+  const titleItem = postItemFragment.querySelector('h5.card-title');
+  if (titleItem) {
+    titleItem.innerText = post.title;
+  }
+
+  // set desc
+  const descItem = postItemFragment.querySelector('.card-text');
+  if (descItem) {
+    descItem.innerText = utils.truncateTextlength(post.description, 110);
+  }
+
+  // set author
+  const authorItem = postItemFragment.querySelector('small:nth-child(2)');
+  if (authorItem) {
+    authorItem.innerText = post.author;
+  }
+
+  // set date
+  const timeItem = postItemFragment.querySelector('small:nth-child(3)');
+  if (timeItem) {
+    const timeString = ` - ${utils.formatDate(post.updatedAt)}`;
+    timeItem.innerText = timeString;
+  }
 
   return postItem;
 };
@@ -30,6 +62,7 @@ const renderPostsList = (postsList) => {
 const init = async () => {
   // Write your logic here ....
   const postList = await postApi.getAll();
+  // console.log(postList);
 
   renderPostsList(postList);
 };
