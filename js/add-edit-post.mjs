0dec3006dde9158db31c9bf40d5e68dae884720a
Duong <duongvu281194@gmail.com>
2019-06-26 17:40:42 +0700

    set values post

--- js/add-edit-post.mjs
+++ js/add-edit-post.mjs
@@ -1,10 +1,33 @@
+'use strict';
+import postApi from './api/postApi.js';
+import utils from "./utils.js";
+
+
+const setValuesPost = (postInfo) => {
+    // set title
+    utils.setValueByElementId('postTitle', postInfo.title);
+
+    // set author
+    utils.setValueByElementId('postAuthor', postInfo.author);
+
+    // set desc
+    utils.setValueByElementId('postDescription', postInfo.description);
+
+    // set img
+    utils.setBackgroundImageByElementId('postHeroImage', postInfo.imageUrl);
+} 
 // -----------------------
 // MAIN LOGIC
 // -----------------------
 const init = async () => {
     // Write your logic here ....
     // console.log('edit');
-  
+
+    const params = new URLSearchParams(window.location.search);
+    const postId = params.get('postId');
+    const postInfo = await postApi.getDetail(postId);
+    setValuesPost(postInfo);
+    //  console.log(postInfo);
 };
 
 init();
